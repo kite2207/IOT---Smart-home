8e399a74eafa365a835f41cf554c54b203b8cecb
KoKoNut <hoc.lnq6@gmail.com>
2026-08-05 13:55:41 +0700

    feat:resolve merge

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,4 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
-
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
-const char* mqtt_server = "broker.hivemq.com";
-const char* mqtt_topic = "safehome/frontdoor/ultrasonic";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-const int trigPin = 5;
-const int echoPin = 18;
-
-void setup_wifi()
-{
-  delay(10);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-  }
-}
-
-void reconnect()
-{
-  while (!client.connected()) {
-    if (client.connect("ESP32_SafeHome_Client")) {
-      // Thành công
-    }
-    else
-    {
-      delay(5000);
-    }
-  }
-}
 
 #include "config.h"
 #include "display.h"
