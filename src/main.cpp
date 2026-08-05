60e23ef44a26a25eed8dc1992dbf26d44b0f55db
nhblong2418@clc.fitus.edu.vn <nhblong2418@clc.fitus.edu.vn>
2026-08-05 07:52:45 +0700

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 
 void setup() {
     Serial.begin(115200);
+    delay(2000);
+    Serial.flush();
+    Serial.println("UART 115200 OK");
     Serial.println("SafeHome Start!");
+    Serial.println("Waiting for data...");
 }
 
 void loop() {
-    delay(1000);
+    static unsigned long lastPrint = 0;
+
+    if (millis() - lastPrint >= 1000) {
+        lastPrint = millis();
+        Serial.println("System running...");
+    }
 }
