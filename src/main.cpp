4e143197329147a15f7e6c79fbd2288995d10530
nhblong2418@clc.fitus.edu.vn <nhblong2418@clc.fitus.edu.vn>
2026-08-06 17:42:03 +0700

    Ultrasonic

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 #include "led.h"
 #include "mqtt_manager.h"
 #include "wifi_manager.h"
+#include "ultrasonic.h"
+
+const int ultrasonic_trigPin = 26;
+const int ultrasonic_echoPin = 27;
+unsigned long ultrasonic_lastMsg = 0;
 
 void setup() {
     Serial.begin(115200);
@@ -20,5 +25,14 @@ void setup() {
 void loop() {
     keepWifiConnected();
     mqttLoop();
+
+    unsigned long now = millis();
+    if (now - ultrasonic_lastMsg > 1000)
+    {
+        ultrasonic_lastMsg = now;
+        String status = get_ultrasonic_status(ultrasonic_trigPin, ultrasonic_echoPin);
+        publishMessage("safehome/ultrasonic", status.c_str());
+    }
+
     delay(10);
 }
